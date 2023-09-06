65f15ea27d39f13ad8a4986f455b5cf4d626f142
shiva-priya04 <144202991+shiva-priya04@users.noreply.github.com>
2023-09-06 11:11:12 +0530

    Add files via upload

---
+++ challenges unit-1/main.py
@@ -0,0 +1,8 @@
+#factorial using recursive
+n=int(input("enter a no.:"))
+def fact(n):
+  if n==0:
+    return 1
+  else:
+    return n*fact(n-1)
+print(fact(n))
---
+++ challenges unit-1/main2.py
@@ -0,0 +1,6 @@
+#leaf year
+a=int(input("enter a year:"))
+if a%4==0:
+  print("Yes it is a leap year")
+else:
+  print("Not a leap year")
